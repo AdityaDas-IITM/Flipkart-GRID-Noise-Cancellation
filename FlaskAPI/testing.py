c9776881ce62879d549478c73ffe711a5f45ea38
nihalgeorge01 <nihalgeorge01@gmail.com>
2020-08-05 21:35:51 +0530

    Revert "Flask endpoint"
    This reverts commit 0e6ee9a26b8e3e1f159416f717e3cdb88614012a.
    
    Reverted back to Flask API from GUI

--- FlaskAPI/testing.py
+++ FlaskAPI/testing.py
@@ -1,17 +1,11 @@
 import requests
 import json
-from GUI import smallGUI
-import os
 
 headers = {'Content-type' : 'application/json'}
+data = {'input_path' : 'D:\\Github Repos\\Flipkart-GRID-Noise-Cancellation2\\WAV-Inputs\\71.wav', 'output_path' : 'D:\\Github Repos\\Flipkart-GRID-Noise-Cancellation2\\FlaskAPI\\'}
+
 url = 'http://127.0.0.1:5000/predict'
-#data = {'input_path' : 'D:\\Github Repos\\Flipkart-GRID-Noise-Cancellation2\\WAV-Inputs\\71.wav', 'output_path' : 'D:\\Github Repos\\Flipkart-GRID-Noise-Cancellation2\\FlaskAPI\\'}
-guiobj = smallGUI()
-inputdir = guiobj.inputdirpath + '/'
-outputdir = guiobj.outputdirpath + '/'
+res = requests.post(url, headers=headers, json=data)
+print(res)
 
-
-for music in os.listdir(inputdir):
-    data = {'input_path' : inputdir + music, 'output_path' : outputdir + 'pred_'+music}
-    res = requests.post(url, headers=headers, json=data)
-    print(res)
+#open(data["input_path"], "rb")
