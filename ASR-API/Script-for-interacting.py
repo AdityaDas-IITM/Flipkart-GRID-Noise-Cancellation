18d18e17d3add06e215729c388a3ddc9991f8220
AdityaDas-IITM <me19b194@smail.iitm.ac.in>
2020-08-06 21:58:25 +0530

    Update Script-for-interacting.py

--- ASR-API/Script-for-interacting.py
+++ ASR-API/Script-for-interacting.py
@@ -3,10 +3,25 @@
 Script for interacting with the flipkart ASR API
 """
 import requests
+import os
+import pandas as pd
 
+datafile = pd.DataFrame()
+filenames = []
+transcriptions = []
 headers = {'Authorization' : 'Token 3715119fd7753d33bedbd3c2832752ee7b0a10c7'}
 data = {'user' : '310' ,'language' : 'HI'}
-files = {'audio_file' : open('Outputs/0_output(4).wav','rb')}
 url = 'https://dev.liv.ai/liv_transcription_api/recordings/'
-res = requests.post(url, headers = headers, data = data, files = files)
-content = res.json()['transcriptions']
+
+folder_path = 'D:\Github Repos\Flipkart-GRID-Noise-Cancellation2\ASR-API\Outputs\\'
+for music in os.listdir(folder_path):
+
+    files = {'audio_file' : open(folder_path + music,'rb')}
+    res = requests.post(url, headers = headers, data = data, files = files)
+    content = (res.json()['transcriptions'])[0]['utf_text']
+    filenames.append(music)
+    transcriptions.append(content)
+
+datafile['Filename'] = filenames
+datafile['Transcriptions'] = transcriptions
+datafile.to_excel('D:\Github Repos\Flipkart-GRID-Noise-Cancellation2\ASR-API\\transcriptions.xlsx', index = False)
