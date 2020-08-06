9712fa1d64a625502ae61872817c2bb8546668e4
nihalgeorge01 <nihalgeorge01@gmail.com>
2020-08-07 01:13:49 +0530

    Obtained ASR Transcriptions

--- ASR-API/Script-for-interacting.py
+++ ASR-API/Script-for-interacting.py
@@ -13,9 +13,10 @@ headers = {'Authorization' : 'Token 3715119fd7753d33bedbd3c2832752ee7b0a10c7'}
 data = {'user' : '310' ,'language' : 'HI'}
 url = 'https://dev.liv.ai/liv_transcription_api/recordings/'
 
-folder_path = 'D:\Github Repos\Flipkart-GRID-Noise-Cancellation2\ASR-API\Outputs\\'
-for music in os.listdir(folder_path):
-
+folder_path = 'C:/Users/nihal/Downloads/FlipkartNoiseCancel/Round3/Key Guidelines and Audio Recording Updated/Flipkart-GRID-Noise-Cancellation/predictions/'
+for music in sorted(os.listdir(folder_path)):
+    print(music)
+    '''
     files = {'audio_file' : open(folder_path + music,'rb')}
     res = requests.post(url, headers = headers, data = data, files = files)
     content = (res.json()['transcriptions'])[0]['utf_text']
@@ -23,5 +24,13 @@ for music in os.listdir(folder_path):
     transcriptions.append(content)
 
 datafile['Filename'] = filenames
-datafile['Transcriptions'] = transcriptions
-datafile.to_excel('D:\Github Repos\Flipkart-GRID-Noise-Cancellation2\ASR-API\\transcriptions.xlsx', index = False)
+datafile['ASR'] = transcriptions
+datafile.to_excel('C:/Users/nihal/Downloads/FlipkartNoiseCancel/Round3/Key Guidelines and Audio Recording Updated/Flipkart-GRID-Noise-Cancellation/ASR-API/ASRTranscriptions.xlsx', index = False)
+
+ASR_data = pd.read_excel("C:/Users/nihal/Downloads/FlipkartNoiseCancel/Round3/Key Guidelines and Audio Recording Updated/Flipkart-GRID-Noise-Cancellation/ASR-API/ASRTranscriptions.xlsx")
+true_data = pd.read_excel("C:/Users/nihal/Downloads/FlipkartNoiseCancel/Round3/Key Guidelines and Audio Recording Updated/Flipkart-GRID-Noise-Cancellation/ASR-API/TrueTranscriptions.xlsx")
+
+all_data = pd.concat([ASR_data['Filename'], true_data['Transcription '] , ASR_data['ASR']], axis=1)
+all_data.to_excel('C:/Users/nihal/Downloads/FlipkartNoiseCancel/Round3/Key Guidelines and Audio Recording Updated/Flipkart-GRID-Noise-Cancellation/ASR-API/AllData.xlsx', index = False)
+print(len(all_data))
+'''
